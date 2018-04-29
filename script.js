e7f154b0697660e095e2ed3181731241a0e62449
ywang20 <ywang20@wpi.edu>
2018-04-29 17:21:42 -0400

    add js and css

---
+++ script.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function(){
+      $('.parallax').parallax();
+      $('.carousel').carousel();
+  $('.carousel.carousel-slider').carousel({fullWidth: true});
+  $('.button-collapse').sideNav();
+    });
