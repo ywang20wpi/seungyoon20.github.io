f0f627f05d1b6727ebb2d49556586c8573092eee
ywang20 <ywang20@wpi.edu>
2018-04-30 21:31:48 -0400

    new template

--- script.js
+++
@@ -1,7 +0,0 @@
-
-$(document).ready(function(){
-      $('.parallax').parallax();
-      $('.carousel').carousel();
-  $('.carousel.carousel-slider').carousel({fullWidth: true});
-  $('.button-collapse').sideNav();
-    });
